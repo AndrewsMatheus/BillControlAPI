bd5acf333982397148915359af798dbfc40ac4e5
AndrewsMatheus <andrewsm.mariano@gmail.com>
2021-12-27 15:34:15 -0400

    Added compatibility to localhost bill client

--- src/main/java/com/study/billsControl/billsPaymentAPI/controller/BillController.java
+++ src/main/java/com/study/billsControl/billsPaymentAPI/controller/BillController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/bills")
+@CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class BillController {
 
